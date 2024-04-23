a4d061a3124bbce2c9bfd7510e5a30745f0acebd
mgodje <114205884+mgodje@users.noreply.github.com>
2024-04-23 12:37:45 -0700

    comitting for now

--- Maryann_Godje_Assignment_5A/main.js
+++ Maryann_Godje_Assignment_5A/main.js
@@ -1,7 +1,9 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
 
 function main() {
-
 	const canvas = document.querySelector( '#c' );
 	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
@@ -12,31 +14,99 @@ function main() {
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 	camera.position.z = 2;
 
+	const controls = new OrbitControls( camera, canvas );
+	controls.target.set( 0, 0, 0 );
+	controls.update();
+
 	const scene = new THREE.Scene();
+	// scene.background = new THREE.Color( 'black' );
+
+	const loader = new THREE.TextureLoader();
+
+	const texture = loader.load( 'sheep.jpg' );
+	texture.colorSpace = THREE.SRGBColorSpace;
+
+	{
+		const color = 0xFFFFFF;
+		const intensity = 3;
+		const light = new THREE.DirectionalLight( color, intensity );
+		light.position.set( - 1, 2, 4 );
+		scene.add( light );
+	}
+
+
+	{
+		const mtlLoader = new MTLLoader();
+		mtlLoader.load( 'Mesh_Hen.mtl', ( mtl ) => {
+			mtl.preload();
+			const objLoader = new OBJLoader();
+			objLoader.setMaterials( mtl );
+			objLoader.load( 'Mesh_Hen.obj');
+			} );
+	}
 
 	const boxWidth = 1;
 	const boxHeight = 1;
 	const boxDepth = 1;
 	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
 
-	const material = new THREE.MeshBasicMaterial( { color: 0x44aa88 } ); // greenish blue
+	function makeInstance( geometry, texture, x ) {
+		const material = new THREE.MeshBasicMaterial( {
+			map: objLoader
+		} );
 
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+		const cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+
+		cube.position.x = x;
+
+		return cube;
+	}
+
+	function makeCube( geometry, color, x ) {
+		const material = new THREE.MeshPhongMaterial( { color } );
+		const cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+
+		cube.position.x = x;
+
+		return cube;
+	}
+
+	function texturedCube( geometry, texture, x ) {
+		const material = new THREE.MeshBasicMaterial( {
+			map: texture
+		} );
+		const cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+
+		cube.position.x = x;
+
+		return cube;
+	}
+
+	const cubes = [
+		makeCube( geometry, 0x44aa88, 0 ),
+		texturedCube( geometry, texture, - 2 ),
+		makeInstance( geometry, objLoader, 2 ),
+	];
 
 	function render( time ) {
-
 		time *= 0.001; // convert time to seconds
+		cubes.forEach( ( cube, ndx ) => {
 
-		cube.rotation.x = time;
-		cube.rotation.y = time;
+			const speed = 1 + ndx * .1;
+			const rot = time * speed;
+			cube.rotation.x = rot;
+			cube.rotation.y = rot;
 
+		} );
 		renderer.render( scene, camera );
 
 		requestAnimationFrame( render );
-
 	}
 	requestAnimationFrame( render );
 }
 
 main();
+
