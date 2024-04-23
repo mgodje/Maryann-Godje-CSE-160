99b5245c7a7e79817eb0b25ce46b593c0fbeed90
mgodje <114205884+mgodje@users.noreply.github.com>
2024-04-23 16:23:52 -0700

    three.js-master included

--- Maryann_Godje_Assignment_5A/main.js
+++ Maryann_Godje_Assignment_5A/main.js
@@ -3,6 +3,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
 
+let OBJECTS = [];
+
 function main() {
 	const canvas = document.querySelector( '#c' );
 	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
@@ -26,38 +28,33 @@ function main() {
 	const texture = loader.load( 'sheep.jpg' );
 	texture.colorSpace = THREE.SRGBColorSpace;
 
-	{
-		const color = 0xFFFFFF;
-		const intensity = 3;
-		const light = new THREE.DirectionalLight( color, intensity );
-		light.position.set( - 1, 2, 4 );
-		scene.add( light );
-	}
+	const color = 0xFFFFFF;
+	const intensity = 3;
+	const light = new THREE.DirectionalLight( color, intensity );
+	light.position.set( - 1, 2, 4 );
+	scene.add( light );	
 
-
-	{
-		const mtlLoader = new MTLLoader();
-		mtlLoader.load( 'Mesh_Hen.mtl', ( mtl ) => {
-			mtl.preload();
-			const objLoader = new OBJLoader();
-			objLoader.setMaterials( mtl );
-			objLoader.load( 'Mesh_Hen.obj');
-			} );
-	}
+	const mtlLoader = new MTLLoader();
+	const objLoader = new OBJLoader();
+	mtlLoader.load( 'Mesh_Hen.mtl', ( mtl ) => {
+		mtl.preload();
+		objLoader.setMaterials( mtl );
+		objLoader.load( 'Mesh_Hen.obj');
+		} );
 
 	const boxWidth = 1;
 	const boxHeight = 1;
 	const boxDepth = 1;
 	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
 
-	function makeInstance( geometry, texture, x ) {
+	function makeObj( geometry, objLoader, x ) {
 		const material = new THREE.MeshBasicMaterial( {
 			map: objLoader
 		} );
 
 		const cube = new THREE.Mesh( geometry, material );
 		scene.add( cube );
-
+		OBJECTS.push(cube);
 		cube.position.x = x;
 
 		return cube;
@@ -88,7 +85,7 @@ function main() {
 	const cubes = [
 		makeCube( geometry, 0x44aa88, 0 ),
 		texturedCube( geometry, texture, - 2 ),
-		makeInstance( geometry, objLoader, 2 ),
+		makeObj( geometry, objLoader, 2 ),
 	];
 
 	function render( time ) {
